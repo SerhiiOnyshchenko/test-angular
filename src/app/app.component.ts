9b07a3afd0b3df7b30528c2c5d225035c46d3280
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-04-30 21:56:31 +0300

    add api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyapidataService } from './currencyapidata.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my first app';
+	title = 'my first app';
+	currjson: any = []
+	
+	from = "USD"
+	to = "USD"
+	result1: string = "1"
+	result2: string = "1"
+
+	changefrom(a: string, b: string, in1: string ) {
+		this.from = a
+		this.to = b
+		this.currency.getcurrencydata(this.from, this.to).subscribe(data => {
+			this.currjson = JSON.stringify(data)
+			this.currjson = JSON.parse(this.currjson)
+			this.result2 = String(Number(this.currjson.result)*Number(in1))
+		})
+	}
+	changeto(a: string, b: string, in2: string ) {
+		this.from = b
+		this.to = a
+		this.currency.getcurrencydata(this.from, this.to).subscribe(data => {
+			this.currjson = JSON.stringify(data)
+			this.currjson = JSON.parse(this.currjson)
+			this.result1 = String(Number(this.currjson.result)*Number(in2))
+		})
+	}
+	constructor(private currency: CurrencyapidataService){}
 }
--- src/assets/scripts/myApp.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function ($scope) {
-  $scope.value1 = 0;
-  $scope.value2 = $scope.value1;
-});
-console.log(value1);
